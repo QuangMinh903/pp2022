93cd09f3c50e37080e87c4e92464451a794795c6
QuangMinh903 <qmguyen2000@gmail.com>
2022-03-29 08:55:56 +0700

    Practical 2 Completed

--- .idea/2.student.mark.oop.py
+++ .idea/2.student.mark.oop.py
@@ -1,66 +1,154 @@
-def getStudentNum ():
-    return int (input ("Enter number of students: "))
+class Stu:
 
-def getCourseNum ():
-    return int (input ("Enter number of courses: "))
+    def input(self, stuLabel=None):
+        if stuLabel is not None:
+            print("Type student's information {} ".format(studentLabel))
+        self.sid = input(f"Enter id: ")
+        self.sname = input("Enter name: ")
+        self.studentdob = input("Enter date of birth: ")
 
-def getCourseDetails ():
-    courseid = input ("Enter course id: ")
-    coursename = input ("Enter course name: ")
-    return courseid, coursename
+    def print(self):
+        print("Student's id: {}".format(self.sid))
+        print("Student's name: {}".format(self.sname))
+        print("Student's date of birth: {}".format(self.studentdob))
 
-def getStudentDetails ():
-    studentid = input ("Enter student id: ")
-    studentname = input ("Enter student name: ")
-    studentdob = input ("Enter student's date of birth: ")
-    return studentid, studentname, studentdob
 
-def getMarks (studentid, courseid):
-    prompt = f"Enter marks for student {studentid} for course {courseid}: ".format (studentid, courseid)
-    input (prompt)
+class Cour:
+    def __init__(self):
+        self.mark = []
 
-nStudents = getStudentNum ()
-studentList = []
-for i in range (nStudents):
-    studentid, nm, dob = getStudentDetails ()
-    studentLst.append ((studentid, nm, dob))
+    def input(self, courLabel=None):
+        if courLabel is not None:
+            print("Type course's information {} ".format(courseLabel))
+        self.cid = input(f"Enter course's id: ")
+        self.sname = input("Enter student's name: ")
 
-nCourses = getCourseNum ()
-courseList = []
-for i in range (nCourses):
-    courseid, coursename = getCourseDetails ()
-    courseList.append ((courseid, coursename))
+    def print(self):
+        print("Course's id: {}".format(self.cid))
+        print("Course's name: {}".format(self.sname))
 
-d = {}
-n = int (input ("Enter how many student-course marks do you want to enter? "))
-for i in range (n):
+    def addMa(self, stu, mark):
+        self.mark.append({
+            'stu': stu,
+            'ma': mark
+        })
+
+    def printMa(self):
+        tmp = {}
+        for mark in self.mark:
+            if mark['stu'] not in tmp.keys():
+                tmp[mark['stu']] = [str(mark['ma'])]
+            else:
+                tmp[mark['stu']].append(str(mark['ma']))
+        for key, value in tmp.items():
+            print("Student {} have {} mark is: {}".format(
+                key, len(value), ", ".join(value)))
+
+class Class:
+    def __init__(self):
+        self.stuList = []
+        self.courList = []
+
+    def inputCour(self):
+        n = int(input("Enter the number of courses: "))
+        for i in range(n):
+            cour = Cour()
+            cour.input(i+1)
+            self.courList.append(cour)
+
+
+    def inputStu(self):
+        n = int(input("Enter the number of students: "))
+        for i in range(n):
+            stu = Stu()
+            stu.input(i+1)
+            self.stuList.append(stu)
+
+    def Stunumb(self):
+        return len(self.stuList)
+
+    def Cournumb(self):
+        return len(self.courList)
+
+    def inputMa(self):
+        courIdx = None
+        while True:
+            courId = input("Enter course id: ")
+            courIdx = self.__checkCour(courId)
+            if courIdx is None:
+                print("Course does not exist")
+            else:
+                break
+        n = int(input("Enter how many student do you want to fill marks? "))
+        for i in range(n):
+            studentId = ""
+            while True:
+                studentId = input("Enter student id {}: ".format(i+1))
+                if not self.__checkValidStudent(studentId):
+                    print("Student does not exist")
+                else:
+                    break
+            mark = int(input("Enter marks: "))
+            courseIdx.addMark(studentId, mark)
+
+    def listStu(self):
+        print("Class have {} students".format(self.Stunumb()))
+        for stu in self.stuList:
+            stu.print()
+            print("")
+
+    def listCour(self):
+        print("Class have {} courses".format(self.Cournumb()))
+        for cour in self.courList:
+            cour.print()
+            print("")
+
+    def showMarkByCourse(self):
+        while True:
+            courId = input("Enter course id: ")
+            cour = self.__checkCour(courId)
+            if courId is None:
+                print("Course does not exist")
+            else:
+                cour.printMa()
+
+    def __checkCour(self, courId):
+        for cour in self.courList:
+            if courId == cour.id:
+                return cour
+        return None
+
+    def __checkStudent(self, stuId):
+        for stu in self.stuList:
+            if stuId == stu.id:
+                return True
+        return False
+
+    _class = Class()
+    _class.inputStu()
+    _class.inputCour()
     while True:
-        studentid = input ("Enter student id: ")
-        courseid = input ("Enter course id: ")
-        if studentid not in [student [0] for student in studentLst]:
-            print ("Bad student id")
-            continue
-        if courseid not in [course [0] for course in courseLst]:
-            print ("Bad course id")
-            continue
-        break
-    marks = int (input ("Enter marks: "))
-    if courseid in d:
-        d [courseid].append ((studentid, marks))
-        print(type(d [courseid]))#list
-    else:
-        d [courseid] = [(studentid, marks)]
-        print(type(courseid))#cid is string
+        print("Choose 1: To fill student's mark with student ID and course ID")
+        print("Choose 2: To list all students")
+        print("Choose 3: To list all courses")
+        print("Choose 4: To list all mark in a course")
+        print("Choose 5: To exit")
 
-print ("\nListing all students now..")
-for s in studentList:
-    print (f"Student id: {s[0]} Name: {s[1]} Date of birth: {s[2]}")
+        n = int(input("Please choose? "))
+        if n == 1:
+            _class.inputMa()
+        elif n == 2:
+            _class.listStu()
+        elif n == 3:
+            _class.listCour()
+        elif n == 4:
+            _class.printMa()
+        elif n == 5:
+            break
+        else:
+            print("PLease choose again from 1-5")
+    return
 
-print ("\nListing all courses now..")
-for c in courseList:
-    print (f"Course id: {c[0]} Name: {c[1]}")
 
-courseid = input ("\nWhich course do you want to see all student marks for? ")
-if courseid in d:
-    for tups in d [courseid]:
-        print (f"Student {tups[0]} got {tups [1]} marks")
+if __name__ == '__main__':
+    main()
